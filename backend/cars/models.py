575bd6a8c880f66066652c72815f14a092b20a87
Joao <joaopedrolima0115@gmail.com>
2026-05-19 16:29:20 -0300

    Inicio do backend

---
+++ backend/cars/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class Marca (models.Model):
+    nome = models.CharField(max_length = 100, unique = True, verbose_name = "Nome da Marca")
+    
+    def __str__ (self):
+        return self.nome
+    
+    class Meta: 
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+        ordering = ["nome"]
+
+class Carro (models.Model):
+    nome = models.CharField(max_length = 100, unique = True)
+    
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT,
+    related_name ="carros",verbose_name ="Marca"
+    )
+    
+    modelo = models.CharField(max_length=100, verbose_name = "Modelo")
+    ano = models.PositiveIntegerField(verbose_name ="Ano")
+    cor = models.CharField(max_length = 20, verbose_name ="Cor")
+    preco = models.DecimalField(max_digits =10, decimal_places =2, verbose_name="Preço")
+    quilometragem = models.PositiveIntegerField(verbose_name="Quilometragem")
+    descricao = models.TextField(blank=True,verbose_name="Descrição")
+    imagem = models.ImageField(upload_to="carros/", blank= True, null =True,verbose_name ="Imagem")
+    
+    anunciante = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, # se o usuário for deletado, tbm deleta os anuncios
+    related_name ="Carros_anunciados", verbose_name ="Anunciante") 
+    criado_em = models.DateTimeField(auto_now_add =True, verbose_name="Criado em")
+    atualizado_em = models.DateTimeField(auto_now =True, verbose_name="Atualizado em")
+    
+    
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.ano})"
+    
+    class Meta:
+        verbose_name = "Carro"
+        verbose_name_plural = "Carros"
+        ordering = ["-criado_em"]
