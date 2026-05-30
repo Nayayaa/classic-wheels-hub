f5860e90bf1b6f420faa59293ba860ed90ba6118
Joao <joaopedrolima0115@gmail.com>
2026-05-30 03:21:10 -0300

    fix(backend): corrige url

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import AnuncioViewSet, ,PortalViewSet
+from .views import AnuncioViewSet, PortalViewSet
 
 router = DefaultRouter()
 router.register(r'anuncios', AnuncioViewSet, basename='anuncio')
